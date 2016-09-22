b153e8e5e5f4d9d780c24f93e4f0fa53ff73c3a5
CodeDiablos <codediablos@gmail.com>
2016-09-22 15:39:54 +0800

    Update 001.Two Sum.py

--- 001.Two Sum.py
+++ 001.Two Sum.py
@@ -10,3 +10,20 @@ class Solution(object):
                 if (nums[i] + nums[(j + i + 1)]) == target:
                     return [i , j + i + 1]
         return []
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        maps = {}
+        i = 0
+        while i < len(nums):
+            tmp = target - nums[i]
+            if tmp in maps:
+                return [maps[tmp], i]
+            maps[nums[i]] = i
+            i += 1
+        return []
