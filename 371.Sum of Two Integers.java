55a6ff455859257f4537646f0f447911cf0c73b8
CodeDiablos <codediablos@gmail.com>
2016-10-03 17:02:41 +0800

    Create 371.Sum of Two Integers.java

---
+++ 371.Sum of Two Integers.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int t = a ^ b;
+            b = ((a & b) << 1);
+            a = t;
+        }
+        return a;
+    }
+}
