80c2b2b4eba5f2d0cf36be5f1df4c2f682547965
CodeDiablos <codediablos@gmail.com>
2016-11-07 11:00:15 +0800

    Create 041.First Missing Positive.py

---
+++ 041.First Missing Positive.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            while nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[nums[i] - 1]:
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        return len(nums) + 1
