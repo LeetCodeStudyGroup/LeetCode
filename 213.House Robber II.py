78daa9797c5fd4714c6c508a64957ddd7e090250
CodeDiablos <codediablos@gmail.com>
2016-11-23 10:52:05 +0800

    Update 213.House Robber II.py

--- 213.House Robber II.py
+++ 213.House Robber II.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.do_rob(nums, 0, len(nums) - 1), self.do_rob(nums, 1, len(nums)))
+
+    def do_rob(self, nums, start, size):
+        include = exclude = 0
+        for i in range(start, size):
+            inc, exc = include, exclude
+            include, exclude = exclude + nums[i], max(inc, exc)
+        return max(include, exclude)
+
+    def rob2(self, nums):
         if len(nums) == 0:
             return 0
         elif len(nums) == 1:
