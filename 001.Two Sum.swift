b3869c26c345e9cba9669749431a529e4e4cd39d
JustBasic101 <felix.ty.kuo@gmail.com>
2017-04-18 12:37:49 +0800

    Update 001.Two Sum.swift

--- 001.Two Sum.swift
+++ 001.Two Sum.swift
@@ -1,6 +1,6 @@
 class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
+    // Approach #1 (Brute Force)
+    func twoSum_1(_ nums: [Int], _ target: Int) -> [Int] {
         for i in 0..<nums.count {
             for j in i+1..<nums.count{
                 if nums[i] + nums[j] == target {
@@ -8,7 +8,40 @@ class Solution {
                 }
             }
         }
+        
+        return []
+    }
     
+    // Approach #2 (Two-pass Hash Table)
+    func twoSum_2(_ nums: [Int], _ target: Int) -> [Int] {
+        var map = Dictionary<Int, Int>()
+        
+        for (index, num) in nums.enumerated() {
+            map[num] = index
+        }
+   
+        for (index, num) in nums.enumerated() {
+            let complement = target - num
+            if map[complement] != nil && map[complement] != index {
+                    return [index, map[complement]!]
+            }
+        }
+        
+        return []
+    }
+    
+    // Approach #3 (One-pass Hash Table)
+    func twoSum_3(_ nums: [Int], _ target: Int) -> [Int] {
+        var map = Dictionary<Int, Int>()
+
+        for (index, num) in nums.enumerated() {
+            let complement = target - num
+            if map[complement] != nil && map[complement] != index {
+                return [index, map[complement]!]
+            }
+            map[num] = index
+        }
+        
         return []
     }
 }
