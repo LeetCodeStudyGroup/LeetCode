daa02204be0c265cf4092f8e6a21af7227a29c17
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-04-18 15:44:54 +0800

    Create 268.Missing Number.cpp

---
+++ 268.Missing Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = nums.size();
+        for (int i=0; i<nums.size(); i++) {
+            sum += i;
+            sum -= nums[i];
+        }
+        
+        return sum;
+    }
+};
