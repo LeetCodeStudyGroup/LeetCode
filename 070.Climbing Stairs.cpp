ddb5d752de1b305c233d4e36de02f3d7326efe7d
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-04-19 17:54:52 +0800

    Update and rename 070.Climbing Stairs to 070.Climbing Stairs.cpp

--- 070.Climbing Stairs.cpp
+++ 070.Climbing Stairs.cpp
@@ -29,3 +29,21 @@ public:
         return count; 
     }
 };
+
+// ============= improve =================
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+        vector<int> mem(n,0);
+        mem[0] = 1;
+        mem[1] = 2;
+        for(int i = 2; i < n; i++) {
+            // cout << mem[i] << ", " << mem[i-1] << ", " << mem[i-2] << endl;
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n-1];
+    }
+};
