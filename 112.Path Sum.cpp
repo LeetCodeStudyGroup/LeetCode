0f9f1683672b076d4357a8715dc7100534b804d2
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-05-02 00:49:18 +0800

    Update and rename 112.Path Sum to 112.Path Sum.cpp

--- 112.Path Sum.cpp
+++ 112.Path Sum.cpp
@@ -9,6 +9,37 @@
  */
 class Solution {
 public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL)
+            return false;
+            
+        if (root->left == NULL && root->right == NULL &&root->val - sum == 0)
+            return true;
+        
+        if (root->left != NULL && root->right != NULL)
+            return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+        
+        if (root->left != NULL)
+            return hasPathSum(root->left, sum - root->val);
+            
+        if (root->right != NULL)
+            return hasPathSum(root->right, sum - root->val);
+            
+        return false;
+    }
+};
+// ==============================================================================
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
     bool checkSum(TreeNode* node, int current, int sum) {
         if (node == NULL) {
             if (current == sum)
