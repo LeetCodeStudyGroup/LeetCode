1e0fcc3dadfdb4c84e8309c605b9fba3982200bd
CodeDiablos <codediablos@gmail.com>
2017-07-30 18:09:57 +0800

    Update 052.N-Queens II.py

--- 052.N-Queens II.py
+++ 052.N-Queens II.py
@@ -4,28 +4,25 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        return self.dfs([], set(), n)
+        return self.dfs([], n)
 
-    def canPlace(self, curList, mark, j):
+    def canPlace(self, curList, j):
         left = right = j
         for inx in range(len(curList) - 1, -1, -1):
             left -=1
             right +=1
-            if curList[inx] == left or curList[inx] == right:
+            if curList[inx] == left or curList[inx] == right or curList[inx] == j:
                 return False
         return True
 
-    def dfs(self, curList, mark, n):
+    def dfs(self, curList, n):
         if n == len(curList):
             return 1
 
         cnt = 0
         for j in range(n):
-            if j not in mark:
-                if self.canPlace(curList, mark, j):
-                    curList.append(j)
-                    mark.add(j)
-                    cnt += self.dfs(curList, mark, n)
-                    curList.pop()
-                    mark.remove(j)
+            if self.canPlace(curList, j):
+                curList.append(j)
+                cnt += self.dfs(curList, n)
+                curList.pop()
         return cnt
