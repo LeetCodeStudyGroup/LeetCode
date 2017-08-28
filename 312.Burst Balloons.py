0aef5f65b420ee86f7ddd0924a5b6ce145124a25
CodeDiablos <codediablos@gmail.com>
2017-08-28 16:59:17 +0800

    Create 312.Burst Balloons.py

---
+++ 312.Burst Balloons.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = [1] + nums + [1]
+        dp = [[0] * len(nums) for _ in range(len(nums))]
+        for k in range(2, len(nums)):
+            for left in range(len(nums) - k):
+                right = left + k
+                for i in range(left + 1, right):
+                    dp[left][right] = max(dp[left][right], nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right])
+        return dp[0][-1]
