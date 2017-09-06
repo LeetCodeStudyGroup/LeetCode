e6508e865d145642df572a483bb8d00039f146d9
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-09-06 16:19:51 +0800

    Update 001.Two Sum.java

--- 001.Two Sum.java
+++ 001.Two Sum.java
@@ -1,3 +1,4 @@
+// solution 1
 public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map2 = new HashMap<>();
         for(int i=0; i<nums.length; i++) {
@@ -11,4 +12,16 @@ public int[] twoSum(int[] nums, int target) {
         }
         
         return null;
+    }
+// solution 2
+public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
