10f025c868da7c2eb5efd8bef2744b8185426c4f
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-09-19 14:30:26 +0800

    Create 015.3Sum.java

---
+++ 015.3Sum.java
@@ -0,0 +1,46 @@
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+ Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+*/
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3)
+            return result;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int low = i+1;
+                int high = nums.length-1;
+                int sum = 0 - nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low + 1]) low++;
+                        while (low < high && nums[high] == nums[high - 1]) high--;
+                        low++; high--;
+                    } else if (nums[low] + nums[high] < sum) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+
+
+        return result;
+    }
