33fea8c87bf43354e9559b446d74ac0a113b52ac
Cynthia Pan <cynthia.cy.pan@gmail.com>
2017-10-19 17:49:23 +0800

    Create 043.Multiply Strings.java

---
+++ 043.Multiply Strings.java
@@ -0,0 +1,34 @@
+/*
+    Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+    Note:
+
+    The length of both num1 and num2 is < 110.
+    Both num1 and num2 contains only digits 0-9.
+    Both num1 and num2 does not contain any leading zero.
+    You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+     */
+
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] answer = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + answer[p2];
+
+                answer[p1] += sum / 10;
+                answer[p2] = sum % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int p : answer) {
+            if(sb.length() != 0 || p != 0)
+                sb.append(p);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
