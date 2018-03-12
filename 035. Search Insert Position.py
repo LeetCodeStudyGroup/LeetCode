a84a79946387005b6a9672071cc14d2cfd0f42ef
JustBasic101 <felix.ty.kuo@gmail.com>
2018-03-12 16:27:56 +0800

    Update 035. Search Insert Position.py

--- 035. Search Insert Position.py
+++ 035. Search Insert Position.py
@@ -10,6 +10,7 @@ class Solution(object):
         start = 0
         end = len(nums) - 1
 
+        ## 嘗試找出 target 在數列中的範圍
         while start + 1 < end:
             mid = int(start + (end - start)/2)
 
@@ -22,14 +23,22 @@ class Solution(object):
             elif nums[mid] > target:
                 end = mid
 
+        ## 測試 target 是否在數列中
         if nums[start] == target:
             return start
 
         if nums[end] == target:
             return end
 
-        for index in range(0, len(nums)):
-            if target < nums[index]:
-                return index
-
-        return len(nums)
+        ## 確認 target 不存在數列中
+        # target < 數列最左邊
+        if target < nums[0]:
+            return 0
+        
+        # target > 數列最右邊
+        if target > nums[-1]:
+            return len(nums)
+        
+        # target 在數列中間
+        return end
+    
