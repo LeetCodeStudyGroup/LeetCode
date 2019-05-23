65db971c0f61297e2cd9f2f6fec701b44a62a5d3
JustBasic101 <felix.ty.kuo@gmail.com>
2019-05-23 19:05:11 +0800

    Update 053. Maximum Subarray.py

--- 053. Maximum Subarray.py
+++ 053. Maximum Subarray.py
@@ -7,23 +7,22 @@ class Solution:
         :rtype: int
         """
         
-        sum_array = []
-        for i in range(0, len(nums)):
-            if i == 0:
-                sum_array.append(nums[0])
-            else:
-                sum_array.append(sum_array[i-1] + nums[i])
-             
-        max_subarray = -sys.maxsize -1
-        min_subarray = sys.maxsize
+        if nums is None:
+            return None
         
-        for i in range(0, len(nums)):
-            if sum_array[i] > max_subarray:
-                max_subarray = sum_array[i]
-            
-            max_subarray = max(max_subarray, sum_array[i] - min_subarray)
-            
-            if sum_array[i] < min_subarray:
-                min_subarray = sum_array[i]
-    
-        return max_subarray
+        if len(nums) == 0:
+            return []
+        
+        maxResult = nums[0]
+        tempSum = nums[0]
+
+        for idx in range(1,len(nums)):
+            if tempSum < 0:
+                tempSum = nums[idx]
+            else:
+                tempSum += nums[idx]
+                         
+            if maxResult < tempSum:
+                maxResult = tempSum
+        
+        return maxResult
