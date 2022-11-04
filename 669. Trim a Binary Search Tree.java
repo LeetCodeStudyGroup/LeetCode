97b342b379f36e1108782b782a84a15e5fc1d10c
Cynthia Pan <cynthia.cy.pan@gmail.com>
2022-11-04 23:44:08 +0800

    Create 669. Trim a Binary Search Tree.java

---
+++ 669. Trim a Binary Search Tree.java
@@ -0,0 +1,41 @@
+/**
+Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode head = new TreeNode();
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return root;
+        
+        if (root.val < low) {
+            root = root.right;
+            root = trimBST(root, low, high);
+        } else if (root.val > high) {
+            root = root.left;
+            root = trimBST(root, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        
+        return root;
+    }
+}
