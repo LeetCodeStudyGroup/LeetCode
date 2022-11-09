c6ce2c64941499c7137e21937759a469a3cec11b
Cynthia Pan <cynthia.cy.pan@gmail.com>
2022-11-09 09:01:25 +0800

    Create 198. House Robber.java

---
+++ 198. House Robber.java
@@ -0,0 +1,33 @@
+/**
+     * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+     *
+     * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [1,2,3,1]
+     * Output: 4
+     * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+     * Total amount you can rob = 1 + 3 = 4.
+     * Example 2:
+     *
+     * Input: nums = [2,7,9,3,1]
+     * Output: 12
+     * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+     * Total amount you can rob = 2 + 9 + 1 = 12.
+     * */
+
+
+    public int rob(int[] nums) {
+        int length = nums.length;
+        int[] sum = new int[length + 1];
+        sum[1] = nums[0];
+        for (int i = 2; i <= length; i++) {
+            for (int j = i-2; j>=0; j--)
+                sum[i] = Math.max(sum[j] + nums[i-1], sum[i]);
+        }
+
+        return Math.max(sum[length], sum[length - 1]);
+    }
